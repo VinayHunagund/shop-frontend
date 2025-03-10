ac97d401124670d2e53effbc06e0661d001e3c2a
VinayHunagund <vinayhunagund45@gmail.com>
2025-03-10 14:59:16 +0530

    done

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -116,7 +116,7 @@ export default function Products() {
               className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700"
             >
               <Plus className="h-5 w-5 mr-2" />
-              Add New Product
+              search
             </Link>
           </div>
         </div>
